e642335f3f858a09ebd2b3eceb236fe255ef06cd
Nikita Danilov <mirt@mirt.su>
2019-08-31 17:10:47 +0300

    Cleanup

--- src/main/java/info/vannier/gotha/JFrPlayersManager.java
+++ src/main/java/info/vannier/gotha/JFrPlayersManager.java
@@ -720,18 +720,6 @@ public class JFrPlayersManager extends javax.swing.JFrame implements RatingListC
     private javax.swing.JComboBox<String> cbxRatingList;
     private javax.swing.ButtonGroup grpAlgo;
     private javax.swing.ButtonGroup grpSetRank;
-    private javax.swing.JLabel jLabel1;
-    private javax.swing.JLabel jLabel10;
-    private javax.swing.JLabel jLabel11;
-    private javax.swing.JLabel jLabel12;
-    private javax.swing.JLabel jLabel13;
-    private javax.swing.JLabel jLabel14;
-    private javax.swing.JLabel jLabel2;
-    private javax.swing.JLabel jLabel3;
-    private javax.swing.JLabel jLabel4;
-    private javax.swing.JLabel jLabel5;
-    private javax.swing.JLabel jLabel6;
-    private javax.swing.JLabel jLabel7;
     private javax.swing.JLabel lblPlFin;
     private javax.swing.JLabel lblPlPre;
     private javax.swing.JLabel lblRatingList;
